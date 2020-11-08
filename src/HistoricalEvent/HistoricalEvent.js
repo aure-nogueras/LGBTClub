a6b9a54b4d8e534b75706ed45ca45daaddaffbd9
aure-nogueras <anogueras@correo.ugr.es>
2020-11-08 19:03:57 +0100

    Añadida función que comprueba si la fecha de un evento es válida #39 y relacionado con HU1 #12 y HU8 #20

--- src/HistoricalEvent/HistoricalEvent.js
+++ src/HistoricalEvent/HistoricalEvent.js
@@ -1,20 +1,46 @@
+/*jshint esversion: 6 */
+
 // Clase HistoricalEvent
 class HistoricalEvent{
 	
-	constructor(name, day, month, year, description){
+	// El evento histórico incluye un email 
+	// Alude a la persona que añadió el evento, en el caso de que sea un usuario del sistema
+	
+	constructor(name, day, month, year, description, email){
 		this.name = name;
 		this.day = day;
 		this.month = month;
 		this.year = year;
 		this.description = description;
+		this.email = email;
 	}
 	
-	getName(){
-		return true;
-	}
-	
-	setName(){
-		return true;
+	// Comprobar que la fecha de un evento histórico es válida	
+	isDateValid(){
+		var valid = false;
+		if(this.year < 2021){
+			if(this.month >= 1 && this.month <=12){	
+				if(this.day >= 1 && this.day <= 28){
+					valid = true;
+				}else if(this.day > 28 && this.day <=31){
+					valid = true;
+					if(this.day == 31 && (this.month != 1 && this.month != 3 && this.month != 5 && 					this.month != 7 && this.month != 8 && this.month != 10 && this.month != 12)){
+						valid = false;
+					}
+					if(this.month == 2){
+						if(this.day >= 30){
+							valid = false;
+						}
+						if(this.day == 29 && this.year % 4 != 0){
+							valid = false;
+						}
+					}
+				}
+			}
+		}
+		return valid;
 	}
 	
 }
+
+module.exports = HistoricalEvent;
