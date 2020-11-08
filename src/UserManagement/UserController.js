76c8491824daf9bb208961ae840fe77c26a2d3ea
aure-nogueras <anogueras@correo.ugr.es>
2020-11-08 19:49:53 +0100

    Añadido controlador de usuarios y función para ver si un usuario existe ya #43 relacionada con HU13 #26 y HU6 #18

---
+++ src/UserManagement/UserController.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 6 */
+var user = require("./User.js");
+
+// Controlador de User
+// En principio se usará un array de usuarios
+// Más adelante se conectará con una base de datos de donde se añadirán o eliminarán los elementos
+
+class UserController{
+
+	constructor(){
+		this.usersList = [];
+	}
+	
+	getUsersList(){
+		return this.usersList;
+	}
+	
+	// Comprobar si un usuario existe ya
+	isAdded(person){
+		var added = false;
+		let pos = this.usersList.indexOf(person);
+		if(pos != -1){
+			added = true;
+		}
+		return added;
+	}
+	
+}
+
+module.exports = UserController;
